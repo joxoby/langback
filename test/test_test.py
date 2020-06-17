790af2bf74db00a97065882cef3027c0403043e8
Juan Oxoby <juan@vicarious.com>
2020-06-16 22:52:33 -0700

    Reformat using black

--- test/test_test.py
+++ test/test_test.py
@@ -3,14 +3,14 @@ import nltk
 from urllib import request
 from nltk import word_tokenize
 
+
 def test_test():
 
-  nltk.download('punkt') # for word_tokenize
+    nltk.download("punkt")  # for word_tokenize
 
-  url = "http://www.gutenberg.org/files/2554/2554-0.txt"
-  response = request.urlopen(url)
-  raw = response.read().decode('utf8')
-  tokens = word_tokenize(raw)
+    url = "http://www.gutenberg.org/files/2554/2554-0.txt"
+    response = request.urlopen(url)
+    raw = response.read().decode("utf8")
+    tokens = word_tokenize(raw)
 
-  assert True
-
+    assert True
