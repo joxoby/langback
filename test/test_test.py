42586cd91e788ff0917b4badfa1debe590296fb1
Juan Oxoby <juan@vicarious.com>
2020-06-16 22:29:30 -0700

    Fix test

--- test/test_test.py
+++ test/test_test.py
@@ -3,14 +3,14 @@ import nltk
 from urllib import request
 from nltk import word_tokenize
 
-nltk.download('punkt') # for word_tokenize
+def test_test():
 
-url = "http://www.gutenberg.org/files/2554/2554-0.txt"
-response = request.urlopen(url)
-raw = response.read().decode('utf8')
-tokens = word_tokenize(raw)
+  nltk.download('punkt') # for word_tokenize
 
-print(f"The length of the text is {len(tokens)}")
+  url = "http://www.gutenberg.org/files/2554/2554-0.txt"
+  response = request.urlopen(url)
+  raw = response.read().decode('utf8')
+  tokens = word_tokenize(raw)
 
-assert True
+  assert True
 
